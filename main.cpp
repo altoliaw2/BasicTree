cf65602fb574bea30b43f9a6ebfaeef21b409b4a
altoliaw <altoliaw@hotmail.com>
2021-05-14 17:19:23 +0800

    Basic version

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+template <class CL1>
+class TNode{
+    public: TNode* op_Par;
+    public: TNode* op_LCh;
+    public: TNode* op_RCh;
+    public: CL1 op_Data;
+    public: TNode(CL1 op_Data){
+        op_Par= nullptr;
+        op_LCh= nullptr;
+        op_RCh= nullptr;
+        this->op_Data = op_Data;
+    }
+};
+
+template <class CL1>
+class Tree{
+    public: TNode* op_Root;
+    public: Tree(){
+        op_Root= nullptr;
+    }
+};
+
+int main(){
+    cout << "Hello world!" << endl;
+    return 0;
+}
